2220538188b642a440686b81da90fc59d7e3385f
gstv <gustavodiefenbach@gmail.com>
2024-10-16 14:36:02 -0300

    First commit

---
+++ ImpossibleGrid/ImpossibleGrid/ImpossibleGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpossibleGridApp.swift
+//  ImpossibleGrid
+//
+//  Created by Gustavo Diefenbach on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ImpossibleGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImpossibleGrid/ImpossibleGridTests/ImpossibleGridTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImpossibleGridTests.swift
+//  ImpossibleGridTests
+//
+//  Created by Gustavo Diefenbach on 16/10/24.
+//
+
+import Testing
+@testable import ImpossibleGrid
+
+struct ImpossibleGridTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
